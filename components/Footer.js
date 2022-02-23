9d4fd11f1a0c3d4d5701e8bdbaf5dcabd3690bfc
Olia <olia.nistra@gmail.com>
2022-02-23 19:36:05 +0300

    updated medium

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,7 @@ function	Footer() {
 				<a href={'https://github.com/Rarity-Extended/RarityExtended'} target={'_blank'} rel={'noopener noreferrer'}>{'Original Code'} </a> { ' | '}
 				<a href={'https://degen-vc.gitbook.io/rartiy2+scarcity/'} target={'_blank'} rel={'noopener noreferrer'}>{'Docs'} </a> { ' | '}
 				<a href={'https://rarity.gold/'} target={'_blank'} rel={'noopener noreferrer'}>{'Guide'} </a> { ' | '}
-				<a href={'hhttps://medium.com/scarcity-gameverse/rarity2/home'} target={'_blank'} rel={'noopener noreferrer'}>{'Medium'} </a> { ' | '}
+				<a href={'https://medium.com/scarcity-gameverse'} target={'_blank'} rel={'noopener noreferrer'}>{'Medium'} </a> { ' | '}
 				<a href={'https://paintswap.finance/marketplace/assets/0xcBe3CA2583db2ea2B13737E115A7C94D10D77891/1'} target={'_blank'} rel={'noopener noreferrer'}>{'Paintswap'} </a> 
 			</div>
 		</div>
